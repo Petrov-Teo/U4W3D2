6e42838ee02aaf0e737347680bbbbc41ecb90993
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-08-20 17:59:28 +0200

    Aggiornato con saving,recovery,update

--- GestioneEventi/src/main/java/io/Petrov_Todor/Application.java
+++ GestioneEventi/src/main/java/io/Petrov_Todor/Application.java
@@ -3,7 +3,6 @@ package io.Petrov_Todor;
 import io.Petrov_Todor.dao.GestioneEventiDAO;
 import io.Petrov_Todor.entities.Evento;
 import io.Petrov_Todor.entities.TipoEvento;
-import io.Petrov_Todor.exceptions.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -17,16 +16,56 @@ public class Application {
         EntityManager em = emf.createEntityManager();
         GestioneEventiDAO ev = new GestioneEventiDAO(em);
 
-        Evento eventoPetrovi = new Evento("EventoPetrovi", LocalDate.of(2024, 8, 20), "Festa privata in maschera", TipoEvento.PRIVATO, 50);
+        //CREAZIONE EVENTI
+        Evento eventoPetrovi = new Evento("EventoPetrovi",
+                LocalDate.of(2024, 8, 20),
+                "Festa privata in maschera",
+                TipoEvento.PRIVATO,
+                50);
 
+        Evento eventoMorini = new Evento("Funerale",
+                LocalDate.of(2023, 5, 1),
+                "Evento privato + catering",
+                TipoEvento.PRIVATO,
+                23);
+
+        Evento eventoCalemme = new Evento("Matrimonio Napoletano",
+                LocalDate.of(2023, 4, 5),
+                "Matrimonio tradizionale con musica dal vivo",
+                TipoEvento.PUBBLICO,
+                2860);
+        Evento eventoTeatrale = new Evento("Spettacolo sotto le Stelle",
+                LocalDate.of(2024, 8, 15),
+                "Fornitura e organizzazione spazi esterni",
+                TipoEvento.PUBBLICO,
+                8500);
+
+// DATA SAVING
+/*
         try {
             ev.save(eventoPetrovi);
+            ev.save(eventoMorini);
+            ev.save(eventoCalemme);
+            ev.save(eventoTeatrale);
         } catch (NotFoundException ex) {
             System.out.println(ex.getMessage());
         }
+*/
+//DATA RECOVERY
+/*
+        try {
+            Evento recoveryCalemme = ev.findById(102);
+            System.out.println(recoveryCalemme);
+//DATA UPDATE
 
+            recoveryCalemme.setTitolo("Festa in piazza");
+            recoveryCalemme.setDescrizione("Viva la musica - evento musicale con musica dal vivo in piazza");
+            ev.save(recoveryCalemme);
+            System.out.println(recoveryCalemme);
+        } catch (NotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+*/
 
     }
-
-
 }
